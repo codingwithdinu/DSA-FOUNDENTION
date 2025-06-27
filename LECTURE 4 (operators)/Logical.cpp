b9a5a8f877ff44cf7bc457c7caf70d4ba18323dc
Dinesh Patel <dinesh.24jiaiml037@jietjodhpur.ac.in>
2025-06-27 09:23:44 +0530

    new

---
+++ LECTURE 4 (operators)/Logical.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main(){
+    //logical operations
+    // && logical AND
+    // || logical OR
+    // ! logical NOT
+
+    bool exp1=true;
+    bool exp2=false;
+
+    cout<<(exp1&&exp2)<<endl;  
+    cout<<(exp1||exp2)<<endl;  
+    cout<<(!exp2)<<endl;  
+}
